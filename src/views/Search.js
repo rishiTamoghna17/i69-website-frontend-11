dc3878243e5492aa9d0053a107aa376d1342c3d4
reactjs_dev_35 <reactjs_dev_35@gmail.com>
2023-02-07 22:33:07 +0530

    add hebrew language

--- src/views/Search.js
+++ src/views/Search.js
@@ -31,6 +31,7 @@ const Search = (props) => {
             <ProfileComponents />
             <ProfileComponents />
             <ProfileComponents />
+            
         </div>
     }
     return (
