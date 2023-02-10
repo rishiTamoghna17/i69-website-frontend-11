593b078a3d42199b3c5df11d4ab7fa953e414695
reactjs_dev_35 <reactjs_dev_35@gmail.com>
2023-02-10 21:26:34 +0530

    fix:language add issue

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'ar', 'de', 'el', 'sw', 'it', 'iw', 'ja', 'fa', 'es', 'fr', 'ko', 'nl', 'ru', 'pt_br', 'pt_pt', 'zh_cn', 'zh_tw', 'he', 'pl', 'no', 'sv', 'hr', 'cs', 'sw'],
+    locales: ['ar', 'cs', 'de', 'el', 'en', 'es', 'fa', 'fr', 'he', 'hr', 'it', 'iw', 'ja', 'ko', 'nl', 'no', 'pl', 'pt_br', 'pt_pt', 'ru', 'sv', 'sw', 'zh_cn', 'zh_tw'],
   },
   localePath: path.resolve('./public/locales'),
   defaultNS: 'translation',
