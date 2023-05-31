62322d848bfd76e1d8c80bbef51fd44b2a61bcf7
reactjs 5935 <reactjs5935@gmail.com>
2023-05-31 21:46:40 +0530

    changes

--- src/common/firebaseConfig.js
+++ src/common/firebaseConfig.js
@@ -8,13 +8,13 @@ const config = {
     // appId: "1:157200471557:web:b23ed26407e2650a432e6c",
     // measurementId: "G-Y2CXD1PBD8"
 
-    apiKey: "AIzaSyB235ilfY0lKSHOhwWHqMnXvs31YC6zl9c",
-    authDomain: "i69app-renewed2022.firebaseapp.com",
-    databaseURL: "https://i69app-renewed2022.firebaseio.com",
-    projectId: "i69app-renewed2022",
-    storageBucket: "i69app-renewed2022.appspot.com",
+    apiKey: "AIzaSyBNDQFHOXjOH-AJH_tvgd7FM_IxLNClDRk",
+    authDomain: "i69-13-02-2023.firebaseapp.com",
+    databaseURL: "https://i69-13-02-2023.firebaseio.com",
+    projectId: "i69-13-02-2023",
+    storageBucket: "i69-13-02-2023.appspot.com",
     messagingSenderId: "37873016176",
-    appId: "1:37873016176:web:77a82b9a4d19a437d943b5",
+    appId: "1:223314224724:android:1792c46a676022f215afc1",
     measurementId: "G-QFSQ8TFC75"
     
 };
