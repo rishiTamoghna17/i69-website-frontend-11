ceb0551708473bc3e42fc86a5e4f909052630d57
reactjs 5935 <reactjs5935@gmail.com>
2023-06-03 18:27:23 +0530

    changes for url

--- src/views/Policy.js
+++ src/views/Policy.js
@@ -123,11 +123,7 @@ const Policy = () => {
           <p>{t('Policy.businessStructure')}</p>
         </Grid>
         <Grid item xs={12} sm={2}>
-<<<<<<< HEAD
-          <Button variant="contained" style={{ margin: 50 }} color="secondary" href="https://play.google.com/store/apps/details?id=com.i69.isyxtinine" target="_blank">
-=======
-          <Button variant="contained" style={{ margin: 50 }} color="secondary" href="https://play.google.com/store/apps/details?id=com.i69app.i69app&pli=1" target="_blank">
->>>>>>> 136ba71321dee0c50c1d48eedae06c95a909a1e2
+          <Button variant="contained" style={{ margin: 50 }} color="secondary" href="https://play.google.com/store/apps/details?id=com.i69.isyxtinine&pli=1&pli=1" target="_blank">
             {t('Home.download')}
           </Button>
         </Grid>
